411ab5fd6a49f59f840b71281d475b298602b3ef
klyaba <klyaba1@gmail.com>
2018-01-07 00:36:18 +0400

    Работает: удаление книг Коряво,но работает: добавление книг

--- resources/controller.js
+++ resources/controller.js
@@ -1,3 +1,4 @@
+// Книги по умолчанию
 var books = [{
     image: 'http://www.booksiti.net.ru/books/58932900.jpg',
     info: {
@@ -24,7 +25,8 @@ var books = [{
     }];
 
 function init() {
-    var table = document.getElementById('testTable');
+    // Таблица по умолчанию
+    var table = document.getElementById('bookTable');
     var rows = [];
     for (var i = 0; i < books.length; i++) {
         rows[i] = table.insertRow(i);
@@ -38,33 +40,94 @@ function init() {
                     '<br>' + books[i].info.year + '</p>';
             }
             if (j == 2) {
-                rows[i].insertCell(j).innerHTML = "<button onclick='showEditWin()'>Редактировать</button><button>Удалить</button>";
+                rows[i].insertCell(j).innerHTML = "<button class='edit-btn'>Редактировать</button><button class='del-btn'>Удалить</button>";
             }
+        }
+    }
+
+    //обработка нажатия кнопок в таблице
+    table.onclick = function (event) {
+        event = event || window.event;
+        var target = event.target || event.srcElement;
+
+        //кнопка удаления книги
+        if (target.className == 'del-btn') {
+            var row = target.parentNode.parentNode;
+            this.deleteRow(row.rowIndex);
+        }
+
+        //кнопка редактирования книги
+        if (target.className == 'edit-btn') {
+
         }
     }
 }
 
-
 function addBook() {
-    showAddBookWin()
+    //Добавление книги в массив
+    books[books.length] = {
+        "image": document.getElementById('add-image').value,
+        "info": {
+            "title": document.getElementById("add-title").value,
+            "author": document.getElementById("add-author").value,
+            "year": document.getElementById("add-year").value
+        }
+    }
+
+    // Добавление строки в таблицу
+    var table = document.getElementById('bookTable');
+    var newRow = table.insertRow(-1);
+    for (var j = 0; j < 3; j++) {
+        if (j == 0) {
+            newRow.insertCell(j).innerHTML = '<img width = "130" src = "' + document.getElementById('add-image').value + '">';
+        }
+        if (j == 1) {
+            newRow.insertCell(j).innerHTML = '<p>' + books[books.length - 1].info.title +
+                '<br>' + books[books.length - 1].info.author +
+                '<br>' + books[books.length - 1].info.year + '</p>';
+        }
+        if (j == 2) {
+            newRow.insertCell(j).innerHTML = "<button>Редактировать</button><button class='del-btn'>Удалить</button>";
+        }
+    }
 }
 
-function showAddBookWin() {
+function showAddBookForm() {
 
     var darkLayer = document.createElement('div'); // слой затемнения
     darkLayer.id = 'shadow'; // id чтобы подхватить стиль
-    document.body.appendChild(darkLayer); // включаем затемнение
+    darkLayer.className = "temporary";
+    document.body.appendChild(darkLayer); // затемнение фона
 
-    var editWindow = document.createElement('div'); // создание окна добавления книги
-    editWindow.id = 'popupWin';
-    editWindow.className = 'modalWin';
-    editWindow.style.textAlign = 'center'; // стилизация
-    editWindow.innerHTML = '<h2> Добавление книги </h2>\n';
-    document.body.appendChild(editWindow); // добавление окна
+    var addForm = document.createElement('div'); // создание окна добавления книги
+    addForm.id = 'popupWin';
+    addForm.className = 'modalWin';
+    addForm.innerHTML = '<h2> Добавление книги </h2>' +
+        '<p>Наименование</p>' +
+        '<input id = "add-title">' +
+        '<p>Автор</p>' +
+        '<input id = "add-author">' +
+        '<p>Год выпуска</p>' +
+        '<input id = "add-year">' +
+        '<p>Изображение</p>' +
+        '<input id = "add-image">' +
+        '<br><input type="button" value = "Добавить" id = "add-btn" class = "temporary">' +
+        ' <button id = "cancel-btn" class = "temporary">Отмена</button>'
+    ;
+    document.body.appendChild(addForm); // добавление формы
 
-    darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
-        darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
-        editWindow.style.display = 'none'; // делаем окно невидимым
-        return false;
-    };
+    // Обработка клика на фоне или кнопках формы
+    var temporary = document.getElementsByClassName("temporary");
+    [].forEach.call(temporary, function (el) {
+        el.onclick = function () {
+            if (el.id == "add-btn") {
+                addBook();
+            }
+            darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+            addForm.parentNode.removeChild(addForm); // удаление окна
+            return false;
+        }
+    });
+
 }
+
